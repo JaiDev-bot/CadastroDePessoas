f4765bffc8bf3d6bf2a77a9d28ee5538fb725731
Jaiane <jaianesoares700@gmail.com>
2025-04-16 22:43:56 -0300

    Criadoa controller

--- src/main/java/dev/jaiane/CadastroDePessoas/CadastroDePessoasApplication.java
+++ src/main/java/dev/jaiane/CadastroDePessoas/CadastroDePessoasApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CadastroDePessoasApplication {
+public class CadastroDePessoasApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroDePessoasApplication.class, args);
